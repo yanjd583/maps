e3a771520d6b803b7f043341f47752c757eecb06
yanjd583 <581jnr583@gmail.com>
2025-09-08 00:40:38 +0900

    Update 01_Hokkaido.js

--- 01_Hokkaido.js
+++ 01_Hokkaido.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('input[name="option"]').forEach(function(radio) {
+  document.querySelectorAll('input[name="layer"]').forEach(function(radio) {
     radio.addEventListener('change', function() {
       document.getElementById('aaa').style.display = this.value === '1' ? 'block' : 'none';
       document.getElementById('iii').style.display = this.value === '2' ? 'block' : 'none';
       document.getElementById('uuu').style.display = this.value === '3' ? 'block' : 'none';
     });
   });
+
 });
